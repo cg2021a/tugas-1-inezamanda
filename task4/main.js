b25a6e6e4a4a49e5605b8d7daf9c8d08c3a3bffe
Inez Yulia Amanda <inez.amanda16@gmail.com>
2021-11-01 02:32:27 +0800

    feat : add plane texture

--- task4/main.js
+++ task4/main.js
@@ -114,6 +114,29 @@ function main() {
     scene.add(sphere);
   }
 
+  //plane (load texture)
+  {
+    const planeSize = 40;
+
+    const loader = new THREE.TextureLoader();
+    const texture = loader.load("./assets/grass1.jpg");
+    texture.wrapS = THREE.RepeatWrapping;
+    texture.wrapT = THREE.RepeatWrapping;
+    texture.magFilter = THREE.NearestFilter;
+    const repeats = planeSize / 2;
+    texture.repeat.set(repeats, repeats);
+
+    const planeGeo = new THREE.PlaneGeometry(planeSize, planeSize);
+    const planeMat = new THREE.MeshPhongMaterial({
+      map: texture,
+      side: THREE.DoubleSide,
+    });
+    const mesh = new THREE.Mesh(planeGeo, planeMat);
+    mesh.receiveShadow = true;
+    mesh.rotation.x = Math.PI * -0.5;
+    scene.add(mesh);
+  }
+
   //load object land
   {
     const mtlLoader = new MTLLoader();
